fcd77a535ba6a85bba4d4f9bc2b893a162e94753
KanishkJagya1 <118714851+KanishkJagya1@users.noreply.github.com>
2024-04-29 04:14:10 +0530

    merge conflict resolve

--- portal-frontend/src/App.js
+++ portal-frontend/src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Header from './components/Header';
-import Body from './components/body/Main';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+// import './App.css';
+// import Header from './components/Header';
+// import Body from './components/body/Main';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Body />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <Body />
+//     </div>
+//   );
+// }
 
 // import "./styles.css";
 import Home from './components/Home_main/Home/Home';
@@ -46,3 +46,30 @@ export default function App() {
     </>
   );
 }
+
+// import React from "react";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+// import Home from "./components/Report/components/Home";
+// import Contact from "./components/Report/components/Contact";
+// import About from "./components/Report/components/About";
+// import Nav from "./components/Report/components/Nav";
+
+// const App = () => {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Nav />
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/about" component={About} />
+//           <Route exact path="/contact" component={Contact} />
+//         </Switch>
+//       </BrowserRouter>
+//     </>
+//   );
+// };
+
+// export default App;
+
