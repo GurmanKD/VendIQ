cee0b4b05931f13b7588ed7224b4170a3106144b
KanishkJagya1 <118714851+KanishkJagya1@users.noreply.github.com>
2024-04-29 04:34:06 +0530

    requirements.txt file

--- portal-frontend/src/App.js
+++ portal-frontend/src/App.js
@@ -1,15 +1,15 @@
-// import './App.css';
-// import Header from './components/Header';
-// import Body from './components/body/Main';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
+import Header from './components/Header';
+import Body from './components/body/Main';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// function App() {
-//   return (
-//     <div className='App'>
-//       <Header />
-//       <Body />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Body />
+    </div>
+  );
+}
 
 export default App;
